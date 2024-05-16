5abd29f4b45af9cb15d1b03c2fd035ba950bdf07
domaindrivendev <domaindrivendev@gmail.com>
2024-05-16 17:54:07 +0100

    Use ohmyzsh instead of zsh-git-prompt

--- roles/git/files/.zshrc.d/git.sh
+++
@@ -1,3 +0,0 @@
-# Enable zsh-git-prompt for enhanced prompt
-source "$(brew --prefix)/opt/zsh-git-prompt/zshrc.sh"
-PROMPT='%B%~%b$(git_super_status) %# '
---
+++ roles/git/files/.zshrc.d/ohmyzsh.sh
@@ -0,0 +1,5 @@
+# Enable ohmyzsh with git plugin
+export ZSH=$HOME/.oh-my-zsh
+ZSH_THEME="robbyrussell"
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
